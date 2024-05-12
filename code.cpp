86b95c20ec5b3dfc1a5bc9d950362aeef5719630
Emam Hasan <146997735+emamhasan1804@users.noreply.github.com>
2024-05-12 18:58:04 +0600

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define int long long
+#define float double
+#define endl '\n'
+using namespace std;
+/* 
+When I wrote this code ,
+only Allah and I knew what I did.
+   
+Now... only Allah knows
+*/
+
+int32_t main () 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    vector<bool> prime(n+1,true);
+    for(int i=2;i<=n;i++) {
+        if(prime[i]) {
+            for(int j=i+i;j<=n;j+=i) {
+                prime[j]=false;
+            }
+        }
+    }
+    for(int i=2;i<=n;i++) {
+        if(prime[i]) cout<<i<<" ";
+    }
+    return 0;
+}
